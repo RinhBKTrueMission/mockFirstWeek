12433415526170c096bcbfc316d0fc275cae49e3
rinhtt <trantherinh99@gmail.com>
2022-03-28 15:36:43 +0700

    update so 1

--- src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
+++ src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
@@ -40,23 +40,23 @@ public class EmployeeController {
     public ResponseEntity<Product> setInfo(@RequestBody Product  product){
         return new ResponseEntity<>(repo.save(product), HttpStatus.OK);
     }
-    @PreAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
-    @PutMapping(value = "update/{id}")
-    public ResponseEntity<Product> updateInfo(@PathVariable String id,@RequestBody Product  product){
-        Optional<Product> userOptional=repo.findById(id);
-        return userOptional.map(product1 -> {product.setCode(product1.getCode());
-            return new ResponseEntity<>(repo.save(product),HttpStatus.OK);
-        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
-    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
-    @DeleteMapping(value = "delete/{id}")
-    public ResponseEntity<Product> deleteInfo(@PathVariable String id){
-        Optional<Product> userOptional=repo.findById(id);
-        return userOptional.map(product ->
-        {
-            repo.deleteById(product.getCode());
-            return new ResponseEntity<>(product,HttpStatus.OK);
-
-        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+//    @PreAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
+//    @PutMapping(value = "update/{id}")
+//    public ResponseEntity<Product> updateInfo(@PathVariable String id,@RequestBody Product  product){
+//        Optional<Product> userOptional=repo.findById(id);
+//        return userOptional.map(product1 -> {product.setCode(product1.getCode());
+//            return new ResponseEntity<>(repo.save(product),HttpStatus.OK);
+//        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
+//    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
+//    @DeleteMapping(value = "delete/{id}")
+//    public ResponseEntity<Product> deleteInfo(@PathVariable String id){
+//        Optional<Product> userOptional=repo.findById(id);
+//        return userOptional.map(product ->
+//        {
+//            repo.deleteById(product.getCode());
+//            return new ResponseEntity<>(product,HttpStatus.OK);
+//
+//        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
 }
