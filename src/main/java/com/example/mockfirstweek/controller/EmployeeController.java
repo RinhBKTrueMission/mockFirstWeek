a56faab3628477ef347bb606abbdace88d0ebf07
rinhtt <trantherinh99@gmail.com>
2022-03-28 13:50:53 +0700

    comment code thoi

--- src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
+++ src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
@@ -24,23 +24,23 @@ public class EmployeeController {
         this.repo = repo;
     }
 
-    @PostAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    //@PostAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @GetMapping("/getall")
     public ResponseEntity<Iterable<Product>> getAll(){
        return new ResponseEntity<>(repo.findAll(), HttpStatus.OK);
     }
-    @PostAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    //@PostAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @GetMapping("/getinfo/{id}")
     public ResponseEntity<Product> getInfo(@PathVariable String id){
         Optional<Product> userOptional=repo.findById(id);
         return userOptional.map(product -> new ResponseEntity<>(product,HttpStatus.OK)).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-    @PreAuthorize("hasRole('ROLE_MODERATOR')")
+    //@PreAuthorize("hasRole('ROLE_MODERATOR')")
     @PostMapping(value = "/setInfo")
     public ResponseEntity<Product> setInfo(@RequestBody Product  product){
         return new ResponseEntity<>(repo.save(product), HttpStatus.OK);
     }
-    @PreAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    //@PreAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
     @PutMapping(value = "update/{id}")
     public ResponseEntity<Product> updateInfo(@PathVariable String id,@RequestBody Product  product){
         Optional<Product> userOptional=repo.findById(id);
@@ -48,15 +48,15 @@ public class EmployeeController {
             return new ResponseEntity<>(repo.save(product),HttpStatus.OK);
         }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
-    @DeleteMapping(value = "delete/{id}")
-    public ResponseEntity<Product> deleteInfo(@PathVariable String id){
-        Optional<Product> userOptional=repo.findById(id);
-        return userOptional.map(product ->
-        {
-            repo.deleteById(product.getCode());
-            return new ResponseEntity<>(product,HttpStatus.OK);
-
-        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
+//    //@PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
+//    @DeleteMapping(value = "delete/{id}")
+//    public ResponseEntity<Product> deleteInfo(@PathVariable String id){
+//        Optional<Product> userOptional=repo.findById(id);
+//        return userOptional.map(product ->
+//        {
+//            repo.deleteById(product.getCode());
+//            return new ResponseEntity<>(product,HttpStatus.OK);
+//
+//        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
+//    }
 }
