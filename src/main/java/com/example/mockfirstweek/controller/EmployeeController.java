7c5735e4471d851504b48c26e19ca6db697f61c4
rinhtt <trantherinh99@gmail.com>
2022-03-29 14:03:58 +0700

    cập nhật 14:03 29/3

--- src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
+++ src/main/java/com/example/mockfirstweek/controller/EmployeeController.java
@@ -2,13 +2,10 @@ package com.example.mockfirstweek.controller;
 
 import com.example.mockfirstweek.model.Product;
 import com.example.mockfirstweek.reponsitory.ProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
@@ -40,23 +37,5 @@ public class EmployeeController {
     public ResponseEntity<Product> setInfo(@RequestBody Product  product){
         return new ResponseEntity<>(repo.save(product), HttpStatus.OK);
     }
-//    @PreAuthorize("hasRole('STAFF') or hasRole('MODERATOR') or hasRole('ADMIN')")
-//    @PutMapping(value = "update/{id}")
-//    public ResponseEntity<Product> updateInfo(@PathVariable String id,@RequestBody Product  product){
-//        Optional<Product> userOptional=repo.findById(id);
-//        return userOptional.map(product1 -> {product.setCode(product1.getCode());
-//            return new ResponseEntity<>(repo.save(product),HttpStatus.OK);
-//        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
-//    @PreAuthorize("hasRole('ADMIN') or hasRole('MODERATOR')")
-//    @DeleteMapping(value = "delete/{id}")
-//    public ResponseEntity<Product> deleteInfo(@PathVariable String id){
-//        Optional<Product> userOptional=repo.findById(id);
-//        return userOptional.map(product ->
-//        {
-//            repo.deleteById(product.getCode());
-//            return new ResponseEntity<>(product,HttpStatus.OK);
-//
-//        }).orElseGet(()->new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
+
 }
